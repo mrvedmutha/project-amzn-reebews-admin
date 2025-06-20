997f2f6d940c9e74aa3ee5e4604507cd96a41e33
Shreyans Jain <coolshreyans01@gmail.com>
2025-06-20 10:37:35 +0000

    fix: eslint ts errors fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
